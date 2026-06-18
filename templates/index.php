2ea23e4d7eb574149041a44972991c530a32bcc0
Aleix Quintana Alsius <kinta@communia.org>
2026-06-18 09:38:35 +0200

    Initial commit

---
+++ templates/index.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+use OCP\Util;
+
+Util::addScript(OCA\UserWatch\AppInfo\Application::APP_ID, OCA\UserWatch\AppInfo\Application::APP_ID . '-main');
+Util::addStyle(OCA\UserWatch\AppInfo\Application::APP_ID, OCA\UserWatch\AppInfo\Application::APP_ID . '-main');
+
+?>
+
+<div id="userwatch"></div>
